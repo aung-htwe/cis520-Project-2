fea8f7223e080ee00e73c8a056a481dd218525f9
Aung Htwe <105806021+aung-htwe@users.noreply.github.com>
2025-02-13 14:36:45 -0600

    load PCB function updated
    load PCB implemented. might be buggy, needs testing.

--- src/process_scheduling.c
+++ src/process_scheduling.c
@@ -49,8 +49,39 @@ bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quan
 
 dyn_array_t *load_process_control_blocks(const char *input_file) 
 {
-	UNUSED(input_file);
-	return NULL;
+	// check if file is NULL or bad file name
+	if (input_file == NULL) return NULL;
+	if (strchr(input_file, '\n') != NULL || strcmp(input_file, "\0") == 0 || strcmp(input_file, "") == 0) return NULL;
+
+	// attempt to open input file. If error, return NULL
+	FILE* file = fopen(input_file, "rb");
+	if (file == NULL) return NULL;
+
+	// variables for reading file
+	uint32_t n = 0;
+	int count = 0;
+	char* buffer = (char*) malloc(sizeof(uint32_t));
+	if (buffer == NULL) return NULL;
+
+	// grab n = number of PCBs
+	if (fgets(buffer, sizeof(buffer), file) != NULL)
+		n = (uint32_t)buffer;
+
+	// create array with room for n PCBs
+	dyn_array_t* arr = dyn_array_create((size_t)n, sizeof(ProcessControlBlock_t), NULL);
+
+	// read file line by line. If line represents burst time, record burst in new pcb and push onto return array.
+	while ((fgets(buffer, sizeof(buffer), file) != NULL){
+		if (count % 3 == 0){
+			ProcessControlBlock_t pcb = { .remaining_burst_time = (uint32_t)buffer };
+			dyn_array_push_front(arr, pcb);
+		}
+		count++;
+	}
+
+	// free buffer and return array
+	free(buffer);
+	return arr;
 }
 
 bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
