d7956713a53c97e7d4fecc9a1cb26ab80134914a
Luke Coverdale <lcoverdale@icr-helios.beocat.ksu.edu>
2025-02-13 14:09:37 -0600

    Testing cmake and creating a test case

---
+++ build/CMakeFiles/hw2_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2_test.dir/test/tests.cpp.o"
+  "CMakeFiles/hw2_test.dir/test/tests.cpp.o.d"
+  "hw2_test"
+  "hw2_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
