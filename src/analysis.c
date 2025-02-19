1481ef8bb0229d1e75a29abd4b85463f2002dd47
Aung Htwe <105806021+aung-htwe@users.noreply.github.com>
2025-02-19 14:04:28 -0600

    Add code for FCFS

--- src/analysis.c
+++ src/analysis.c
@@ -19,7 +19,25 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	abort();		// REPLACE ME with implementation.
+	// grab filename of pcb
+	char* fileName = argv[1];
+
+	// grab scheduling algorithm used
+	char* algorithm = argv[2];
+
+	// load ready queue from pcb file. If NULL, return error code -1
+	dyn_array_t* readyQueue = load_process_control_blocks(fileName);
+	if (readyQueue == NULL) return -1;
+
+	ScheduleResult_t *result = (ScheduleResult_t*) malloc(sizeof(ScheduleResult_t));
+
+
+        if (strcmp(algorithm, "FCFS") == 0)
+		first_come_first_serve(readyQueue, result);
+
+	printf("Average waiting time: %f\n", result->average_waiting_time);
+	printf("Average turnaround time: %f\n", result->average_turnaround_time);
+	printf("Total run time: %ld\n", result->total_run_time);
 
 	return EXIT_SUCCESS;
 }
