95b0191f810b7592e2139e7a2f7fde27e26c3142
Luke Coverdale <lcoverdale@icr-clymene.beocat.ksu.edu>
2025-02-20 11:00:02 -0600

    working with analysis.c

--- src/analysis.c
+++ src/analysis.c
@@ -9,6 +9,14 @@
 #define RR "RR"
 #define SJF "SJF"
 
+int new_strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return (unsigned char)(*s1) - (unsigned char)(*s2);
+}
+
 // Add and comment your analysis code in this function.
 // THIS IS NOT FINISHED.
 int main(int argc, char **argv) 
@@ -25,19 +33,37 @@ int main(int argc, char **argv)
 	// grab scheduling algorithm used
 	char* algorithm = argv[2];
 
+	// grab quantum for round-robin
+	size_t quantum = 0;
+	if(argc == 4 && (*argv[3] >= '0' && *argv[3] <= '9')){
+		quantum = quantum * 10 + (*argv[3] - '0');
+	}
+
 	// load ready queue from pcb file. If NULL, return error code -1
 	dyn_array_t* readyQueue = load_process_control_blocks(fileName);
-	if (readyQueue == NULL) return -1;
+	if (readyQueue == NULL) return EXIT_FAILURE;
 
 	ScheduleResult_t *result = (ScheduleResult_t*) malloc(sizeof(ScheduleResult_t));
-
-
-        if (strcmp(algorithm, "FCFS") == 0)
+	if(result == NULL) return EXIT_FAILURE;
+    	if (new_strcmp(algorithm, FCFS) == 0)
 		first_come_first_serve(readyQueue, result);
+		else if(new_strcmp(algorithm, P) == 0)
+		priority(readyQueue, result);
+		else if(new_strcmp(algorithm, RR) == 0)
+		round_robin(readyQueue, result, quantum);
+		else if(new_strcmp(algorithm, SJF) == 0)
+		shortest_job_first(readyQueue, result);
 
-	printf("Average waiting time: %f\n", result->average_waiting_time);
-	printf("Average turnaround time: %f\n", result->average_turnaround_time);
-	printf("Total run time: %ld\n", result->total_run_time);
+	FILE *fptr = fopen("README.md", "w");
+	if(fptr == NULL)
+	return EXIT_FAILURE;
 
+	fprintf(fptr, "Average waiting time: %f\n", result->average_waiting_time);
+	fprintf(fptr, "Average turnaround time: %f\n", result->average_turnaround_time);
+	fprintf(fptr, "Total run time: %ld\n", result->total_run_time);
+
+	fclose(fptr);
+	free(result);
+	dyn_array_destroy(readyQueue);
 	return EXIT_SUCCESS;
 }
