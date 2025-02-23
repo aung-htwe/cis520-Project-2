e92a2bc478c1c6e8e1a5838ef7bb90081eb46f50
Luke Coverdale <lcoverdale@icr-clymene.beocat.ksu.edu>
2025-02-23 14:39:28 -0600

    Added a test for shortest_remaining_time_first

--- test/tests.cpp
+++ test/tests.cpp
@@ -108,7 +108,7 @@ TEST (first_come_first_serve, CorrectProcess){
 	free(output);
 }
 
-/*
+
 TEST(shortest_job_first, InvalidParam){
 	EXPECT_EQ(false, shortest_job_first(NULL,NULL));
 }
@@ -239,8 +239,47 @@ TEST(round_robin, CorrectProcess){
 }
 
 
-
+/*
+* Shortest remaining job first tests. One to check proper param checking,
+* one to check functionality of the program
 */
+TEST(shortest_remaining_time_first, InvalidParam){
+	EXPECT_EQ(false, shortest_remaining_time_first(NULL,NULL));
+}
+
+TEST(shortest_remaining_time_first, CorrectProcess){
+	dyn_array_t *queue = dyn_array_create(3, sizeof(ProcessControlBlock_t), NULL);
+
+	ProcessControlBlock_t *p1 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
+	p1->priority = 0;
+	p1->arrival = 0;
+	p1->remaining_burst_time = 6;
+	p1->started = false;
+	dyn_array_push_back(queue, &p1);
+
+	ProcessControlBlock_t *p2 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
+	p2->priority = 0;
+	p2->arrival = 0;
+	p2->remaining_burst_time = 8;
+	p2->started = false;
+	dyn_array_push_back(queue, &p2);
+	
+	ProcessControlBlock_t *p3 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
+	p3->priority = 0;
+	p3->arrival = 0;
+	p3->remaining_burst_time = 5;
+	p3->started = false;
+	dyn_array_push_back(queue, &p3);
+
+	ScheduleResult_t *output = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+
+	bool result = shortest_remaining_time_first(queue, output);
+
+	assert(result);
+	assert(output->average_turnaround_time == (11.0 + 19.0 + 5.0)/3.0);
+	assert(output->average_waiting_time == (5.0 + 11.0)/3.0);
+}
+
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
