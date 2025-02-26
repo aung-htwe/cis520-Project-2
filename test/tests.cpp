e4b8ed6029fcf53643f1272ecfd133580c02d07a
Luke Coverdale <lcoverdale@icr-clymene.beocat.ksu.edu>
2025-02-25 19:46:42 -0600

    Added strf test

--- test/tests.cpp
+++ test/tests.cpp
@@ -229,21 +229,21 @@ TEST(round_robin, CorrectProcess){
 	p1->arrival = 0;
 	p1->remaining_burst_time = 10;
 	p1->started = false;
-	dyn_array_push_back(queue, &p1);
+	dyn_array_push_back(queue, p1);
 
 	ProcessControlBlock_t *p2 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
 	p2->priority = 0;
 	p2->arrival = 0;
 	p2->remaining_burst_time = 5;
 	p2->started = false;
-	dyn_array_push_back(queue, &p2);
+	dyn_array_push_back(queue, p2);
 	
 	ProcessControlBlock_t *p3 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
 	p3->priority = 0;
 	p3->arrival = 0;
 	p3->remaining_burst_time = 8;
 	p3->started = false;
-	dyn_array_push_back(queue, &p3);
+	dyn_array_push_back(queue, p3);
 
 	ScheduleResult_t *output = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
 
@@ -254,7 +254,42 @@ TEST(round_robin, CorrectProcess){
 	assert(output->average_waiting_time == (13.0 + 10.0 + 13.0)/3.0);
 }
 
+TEST(shortest_remaining_time_first, InvalidParam){
+	EXPECT_EQ(false, shortest_remaining_time_first(NULL,NULL));
+}
 
+TEST(shortest_remaining_time_first, CorrectProcess){
+	dyn_array_t *queue = dyn_array_create(3, sizeof(ProcessControlBlock_t), NULL);
+
+	ProcessControlBlock_t *p1 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
+	p1->priority = 0;
+	p1->arrival = 0;
+	p1->remaining_burst_time = 6;
+	p1->started = false;
+	dyn_array_push_back(queue, p1);
+
+	ProcessControlBlock_t *p2 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
+	p2->priority = 0;
+	p2->arrival = 0;
+	p2->remaining_burst_time = 8;
+	p2->started = false;
+	dyn_array_push_back(queue, p2);
+	
+	ProcessControlBlock_t *p3 = (ProcessControlBlock_t *)malloc(sizeof(ProcessControlBlock_t));
+	p3->priority = 0;
+	p3->arrival = 0;
+	p3->remaining_burst_time = 5;
+	p3->started = false;
+	dyn_array_push_back(queue, p3);
+
+	ScheduleResult_t *output = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+
+	bool result = shortest_remaining_time_first(queue, output);
+
+	assert(result);
+	assert(output->average_turnaround_time == (11.0 + 19.0 + 5.0)/3.0);
+	assert(output->average_waiting_time == (5.0 + 11.0)/3.0);
+}
 
 */
 int main(int argc, char **argv)
