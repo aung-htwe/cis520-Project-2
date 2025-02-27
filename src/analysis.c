e4377058765b4911c0cc0f70fab9bbc4fd6e0546
Aung Htwe <105806021+aung-htwe@users.noreply.github.com>
2025-02-27 11:42:48 -0600

    Update analysis.c

--- src/analysis.c
+++ src/analysis.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "dyn_array.h"
 #include "processing_scheduling.h"
@@ -9,6 +10,10 @@
 #define RR "RR"
 #define SJF "SJF"
 
+// New and safer strcmp
+// \param s1 first string to compare
+// \param s2 second string to compare
+// 0 if strings equal, >0 if s1 greater, <0 if s2 greater
 int new_strcmp(const char *s1, const char *s2) {
     while (*s1 && *s2 && (*s1 == *s2)) {
         s1++;
@@ -17,12 +22,17 @@ int new_strcmp(const char *s1, const char *s2) {
     return (unsigned char)(*s1) - (unsigned char)(*s2);
 }
 
-// Add and comment your analysis code in this function.
-// THIS IS NOT FINISHED.
+// Main function for running analysis to grab stats on scheduling algorithms
+// \param argc number of arguments in command line
+// \param argv pointer to command line arguments
+// return EXIT_SUCCESS if successful, EXIT_FAILURE if error
 int main(int argc, char **argv) 
 {
-	if (argc < 3) 
-	{
+	// change directory to parent directory
+	chdir("..");
+
+	// if insufficient amount of arguments, return EXIT_FAILURE
+	if (argc < 3 || argc > 4) {
 		printf("%s <pcb file> <schedule algorithm> [quantum]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -33,35 +43,49 @@ int main(int argc, char **argv)
 	// grab scheduling algorithm used
 	char* algorithm = argv[2];
 
-	// grab quantum for round-robin
+	// grab quantum for round-robin. Must be valid quantum
 	size_t quantum = 0;
 	if(argc == 4 && (*argv[3] >= '0' && *argv[3] <= '9')){
 		quantum = quantum * 10 + (*argv[3] - '0');
 	}
 
-	// load ready queue from pcb file. If NULL, return error code -1
+	// load ready queue from pcb file. If NULL, return EXIT_FAILURE
 	dyn_array_t* readyQueue = load_process_control_blocks(fileName);
 	if (readyQueue == NULL) return EXIT_FAILURE;
 
+	// create schedule result struct. If unsuccessful, return EXIT_FAILURE
 	ScheduleResult_t *result = (ScheduleResult_t*) malloc(sizeof(ScheduleResult_t));
 	if(result == NULL) return EXIT_FAILURE;
-    	if (new_strcmp(algorithm, FCFS) == 0)
+
+
+	// Open file to print out to. If can't open, return failure
+	FILE *fptr = fopen("README.md", "a");
+	if (fptr == NULL) return EXIT_FAILURE;
+
+	// determine what algorithm was selected and fill result with respective algorithm's stats
+	if (new_strcmp(algorithm, FCFS) == 0){
 		first_come_first_serve(readyQueue, result);
-		else if(new_strcmp(algorithm, P) == 0)
+		fprintf(fptr, "Results for First Come First Serve...................\n");
+	}
+	else if(new_strcmp(algorithm, P) == 0){
 		priority(readyQueue, result);
-		else if(new_strcmp(algorithm, RR) == 0)
+		fprintf(fptr, "Results for Priority.................................\n");
+	}
+	else if(new_strcmp(algorithm, RR) == 0){
 		round_robin(readyQueue, result, quantum);
-		else if(new_strcmp(algorithm, SJF) == 0)
+		fprintf(fptr, "Results for Round Robin..............................\n");
+	}
+	else if(new_strcmp(algorithm, SJF) == 0){
 		shortest_job_first(readyQueue, result);
+		fprintf(fptr, "Results for Shortest Job First.......................\n");
+	}
 
-	FILE *fptr = fopen("README.md", "w");
-	if(fptr == NULL)
-	return EXIT_FAILURE;
-
+	// print stats for scheduling algorithm
 	fprintf(fptr, "Average waiting time: %f\n", result->average_waiting_time);
 	fprintf(fptr, "Average turnaround time: %f\n", result->average_turnaround_time);
-	fprintf(fptr, "Total run time: %ld\n", result->total_run_time);
+	fprintf(fptr, "Total run time: %ld\n\n\n", result->total_run_time);
 
+	// close file, free result and array. Return success
 	fclose(fptr);
 	free(result);
 	dyn_array_destroy(readyQueue);
